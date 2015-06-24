4b33e87acb1dae029d228eae8abd7f76fad28b42
control <control@yahoo-inc.com>
2015-06-25 04:19:28 +0800

    Initial Commit

---
+++ Yelp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Yelp
+//
+//  Created by Curtis Kang on 6/25/15.
+//  Copyright (c) 2015 Curtis Kang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
